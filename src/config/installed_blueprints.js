38b6e5bd45f4b7e22935f19321222b7c194688e4
Crowdbotics <team@crowdbotics.com>
2020-10-05 17:14:49 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -7,7 +7,6 @@ export const installed_blueprints = [
 { name: 'Settings127576', human_name: 'Settings', access_route: 'Settings127576'},
 { name: 'Settings127568', human_name: 'Settings', access_route: 'Settings127568'},
 { name: 'UserProfile127566', human_name: 'User Profile', access_route: 'UserProfile127566'},
-{ name: 'BlankScreen0127544', human_name: 'Blank Screen 0', access_route: 'BlankScreen0127544'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -12,7 +12,6 @@ import NotificationList127577Navigator from '../features/NotificationList127577/
 import Settings127576Navigator from '../features/Settings127576/navigator';
 import Settings127568Navigator from '../features/Settings127568/navigator';
 import UserProfile127566Navigator from '../features/UserProfile127566/navigator';
-import BlankScreen0127544Navigator from '../features/BlankScreen0127544/navigator';
 
 /**
  * new navigators can be imported here
@@ -28,7 +27,6 @@ NotificationList127577: { screen: NotificationList127577Navigator },
 Settings127576: { screen: Settings127576Navigator },
 Settings127568: { screen: Settings127568Navigator },
 UserProfile127566: { screen: UserProfile127566Navigator },
-BlankScreen0127544: { screen: BlankScreen0127544Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
